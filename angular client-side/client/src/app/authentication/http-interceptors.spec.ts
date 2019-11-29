25d45d9f1d0743c0e00fdcea8d846afd0c256b78
roel lucassen <roel.l@live.nl>
2019-11-29 11:13:26 +0100

    change

--- angular client-side/client/src/app/authentication/http-interceptors.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptors } from './http-interceptors';
-
-describe('HttpInterceptors', () => {
-  it('should create an instance', () => {
-    expect(new HttpInterceptors()).toBeTruthy();
-  });
-});
